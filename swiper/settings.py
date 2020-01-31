c5d4098dbe1548dec8ece4b42102766c35387455
Ba-Gai <m15755407160@163.com>
2020-01-31 21:23:42 +0800

    将settings里面的默认数据库改成了mysql，更新pipfile，添加了pymysql，创建user的model里面的字段，然后做数据迁移

--- swiper/settings.py
+++ swiper/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'swiper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'swiper',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '12344321',
+        'PORT': 3306,
     }
 }
 
