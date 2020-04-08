a2c872ed3736b5440a6353d4e9ddae38c05abc62
Ba-Gai <m15755407160@163.com>
2020-04-08 15:24:50 +0800

    更新发布脚本，重启远程服务器命令，新添加了服务器部署脚本deploy

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,3 +5,7 @@ REMOTE_DIR='/opt/swiper/'
 LOCAL_DIR='./'
 
 rsync -crvP --exclude={logs, venv} --delete $LOCAL_DIR $USER@HOST:$REMOTE_DIR
+
+
+# 重启远程服务器
+ssh @$USER@HOST "$REMOTE_DIR/scripts/restart.sh"
